1b7ce6fead31831a3b0001c8844865a7920e049f
naseriboroujeni <naseriboroujeni@gmail.com>
2025-06-06 00:02:28 +0300

    Add user entity

---
+++ services/auth-service/src/main/java/com/taskwise/authservice/model/User.java
@@ -0,0 +1,24 @@
+package com.taskwise.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
