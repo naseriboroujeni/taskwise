27ee5a741e15640b159c24419af265934b9e7866
naseriboroujeni <naseriboroujeni@gmail.com>
2025-06-06 19:22:20 +0300

    Add user repository

---
+++ services/auth-service/src/main/java/com/taskwise/authservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.taskwise.authservice.repository;
+
+import com.taskwise.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+}
